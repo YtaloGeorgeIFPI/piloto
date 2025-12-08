55a59cb9b93d7f0e2ac049cc2bf174b3da2f1c85
Ytalo George <ytalogsilva@gmail.com>
2025-12-08 09:28:54 -0300

    Atualização

--- home/urls.py
+++ home/urls.py
@@ -4,17 +4,27 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    
-    #Rota para pagina Inicial Raiz
+    # Rota para página Inicial Raiz
     path('', views.index, name="index"),
     
-    #Pagina Sobre
+    # Página Sobre
     path('sobre/', views.sobre, name="sobre"),
     
-    #Pagina para Contatos
+    # Página para Contatos
     path("contato/", views.contato, name="contato"), 
     
-    #Pagina de Ajuda
-    path("ajuda/", views.ajuda, name="ajuda")
+    # Página de Ajuda
+    path("ajuda/", views.ajuda, name="ajuda"),  # Aqui foi adicionada a vírgula
+    
+    # Página de Exibição de Item
+    path('item/<int:id>', views.exibir_item, name="exibir_item"),
+
+    # Página de Perfil
+    path('perfil/<str:usuario>', views.exibir_perfil, name="exibir_perfil"),
+
+    # URLs já existentes...
+    path('diasemana/<int:num>/', views.dia_da_semana, name='dia_da_semana'),
+
+
 
 ]
--- home/views.py
+++ home/views.py
@@ -12,3 +12,29 @@ def contato(request):
 
 def ajuda(request):
     return render(request, "ajuda.html")
+
+def exibir_item(request, id):
+    return render(request, "exibir_item.html", {'id':id})
+
+def exibir_perfil(request, usuario):
+    return render(request, "exibir_perfil.html", {'usuario':usuario})
+
+def dia_da_semana(request, num):
+    dias = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado",
+    }
+
+    # Verifica se o número está entre 1 e 7
+    if num < 1 or num > 7:
+        return HttpResponse("Número inválido! Use um número entre 1 e 7.")
+    
+    # Obtém o dia correspondente
+    dia = dias.get(num)
+    return render(request, "dia_da_semana.html", {'dia': dia})
+
